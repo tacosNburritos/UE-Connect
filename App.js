2bfeeb4eb591dcf13081a2ec6bea1b7b9f5c1662
Jinjaaa <jinjer.ico@gmail.com>
2025-02-08 18:38:19 +0800

    front page
    progress for frontpage

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar, StyleSheet, Text, View, Button, SafeAreaView, Platform} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient'; 
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <SafeAreaView style={[styles.container ]}>
-      <StatusBar backgroundColor="grey" barStyle="light-content" translucent={true}/>
+    <LinearGradient style={[styles.container]} colors={['#8E0E00','#1F1C18']}>
+    <SafeAreaView >
+      <StatusBar backgroundColor="#8E0E00" barStyle="light-content" translucent={true}/>
       <WelcomeScreen />
     </SafeAreaView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
