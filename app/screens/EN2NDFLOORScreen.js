37bbf07e28dd3f634902cbd0eddad9482c538e25
tacosNburritos <renzoisaac912@gmail.com>
2025-04-27 03:01:19 +0800

    EN 2nd Floor Reworked
    EN 2nd Floor Reworked

--- app/screens/EN2NDFLOORScreen.js
+++ app/screens/EN2NDFLOORScreen.js
@@ -191,7 +191,7 @@ const EN2NDFLOORScreen = ({ route, navigation }) => {
                 key={`node-${index}`}
                 cx={cx}
                 cy={cy}
-                r={1.5}
+                r={5}
                 fill="red"
                 animatedProps={animatedProps}
               />
